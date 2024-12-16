5e8ab3e154b615638373bdcdfddba80e6dec1c52
dartoverin <70742597+dartoverin@users.noreply.github.com>
2024-12-16 18:42:26 +0100

    Update parser_analizer.c
    I FOUND IT!!!!! IT'S WORKING!!!!!!!!!!!!!!!!!!!!!!

--- new_minishell/lexer_parser/parser_analizer.c
+++ new_minishell/lexer_parser/parser_analizer.c
@@ -12,6 +12,32 @@
 
 #include "minishell.h"
 
+static void	set_pipes(t_list *cmd_list)
+{
+	t_command	*current_cmd;
+	t_command	*next_cmd;
+	int			fds[2];
+
+	while (cmd_list)
+	{
+		current_cmd = (t_command *)cmd_list->content;
+		if (cmd_list->next)
+		{
+			next_cmd = (t_command *)cmd_list->next->content;
+			pipe(fds);
+			if (current_cmd->out_fileno == 0)
+				ft_memcpy(&(current_cmd->out_fileno), fds + 1, sizeof(int));
+			else
+				close(fds[1]);
+			if (next_cmd->in_fileno == 0)
+				ft_memcpy(&(next_cmd->in_fileno), fds + 0, sizeof(int));
+			else
+				close(fds[0]);
+		}
+		cmd_list = cmd_list->next;
+	}
+}
+
 t_error	error_states(t_list *tokens, t_pars_state *current, t_list **cmd_list,
 		t_data *data)
 {
@@ -83,5 +109,6 @@ t_list	*parser_analysis(t_list *tokens, t_data *data)
 		db_error("parser syntax error", 0);
 	if (error_type != NO_ERROR || current_state != P_COMMAND)
 		return (free_command_list(&cmd_list), NULL);
+	set_pipes(cmd_list);
 	return (cmd_list);
 }
